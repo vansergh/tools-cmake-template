64e809e8d05723c682d3e6aa5e5999860f76866f
vansergh <vansergh@yandex.ru>
2024-07-21 20:30:40 +0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <openssl/crypto.h>
-#include "../lib/lib.hpp"
+#include <lib.hpp>
 
 using namespace std;
 
 int main() {
-    int c;
     MyClass1 a;
     MyClass2 b;
     a.SayHello();
